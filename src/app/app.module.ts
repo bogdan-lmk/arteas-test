a03f1b10deeec0408a29ce6620b301439dee0b28
Bogdan Lameko <bogdan.lmk@gmail.com>
2018-11-14 23:43:31 +0200

    main functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SvgIconComponent } from './components/svg-icon.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { AboutpageComponent } from './components/aboutpage/aboutpage.component';
+import { CardComponent } from './components/card/card.component';
+
+
+const appRoutes: Routes =[
+  { path: '', component: HomepageComponent},
+  { path: 'about', component: AboutpageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SvgIconComponent
+    SvgIconComponent,
+    HomepageComponent,
+    AboutpageComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
